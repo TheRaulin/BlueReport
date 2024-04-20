40aab2c160a25dd97cbe8b60d14182530febb232
theraulin <raulbarboza0114@gmail.com>
2024-04-20 00:38:22 -0500

    Pruebas de report

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    const selectTurno = document.getElementById("turno");
+    const buttonReport = document.getElementById("b-report");
+
+
+    buttonReport.addEventListener('click', function() {
+        var selectedTurno = selectTurno.value;
+        console.log(selectedTurno);
+    });
 });
