398a23be82c624b355ed52a01f0d6b16164a1604
TheRaulin <143981951+TheRaulin@users.noreply.github.com>
2024-04-19 23:58:27 -0500

    Add files via upload

---
+++ tailwind.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+      },
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          base: '#0C79BD',
+        }, 
+      }
+    }
+  }
