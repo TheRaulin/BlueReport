0977d7a53c79940c2680bd19e4e1b1e082de3897
theraulin <raulbarboza0114@gmail.com>
2024-04-30 05:50:04 -0500

    Compartidas y privadas terminadas, eventos y datos

---
+++ Js/grecia.js
@@ -0,0 +1,168 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let GreciaIn = 0;
+
+    const bt1dGrecia = document.getElementById("bt1dGrecia");
+    const bt2dGrecia = document.getElementById("bt2dGrecia");
+    const bt3dGrecia = document.getElementById("bt3dGrecia");
+    const bt1Grecia = document.getElementById("bt1Grecia");
+    const bt2Grecia = document.getElementById("bt2Grecia");
+    const bt3Grecia = document.getElementById("bt3Grecia");
+    const bt4Grecia = document.getElementById("bt4Grecia");
+    const bt5Grecia = document.getElementById("bt5Grecia");
+    const bt6Grecia = document.getElementById("bt6Grecia");
+
+
+    //Cama 1 doble Grecia ------------------------------------------------------------ 1 doble
+    bt1dGrecia.addEventListener('click', function() {
+        if(bt1dGrecia.value == 0) {
+            GreciaIn += 1;
+            bt1dGrecia.value = 1;
+            bt1dGrecia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt1dGrecia.value == 1){
+            bt1dGrecia.value = 2;
+            bt1dGrecia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt1dGrecia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            GreciaIn -= 1;
+            bt1dGrecia.value = 0;
+            bt1dGrecia.classList.remove('bg-out', 'text-white', 'border-inBorder');              
+        }
+    });  
+
+    //Cama 2 doble Grecia ----------------------------------------------------------- 2 doble
+    bt2dGrecia.addEventListener('click', function() {
+        if(bt2dGrecia.value == 0) {
+            GreciaIn += 1;
+            bt2dGrecia.value = 1;
+            bt2dGrecia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt2dGrecia.value == 1){
+            bt2dGrecia.value = 2;
+            bt2dGrecia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt2dGrecia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            GreciaIn -= 1;
+            bt2dGrecia.value = 0;
+            bt2dGrecia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });    
+
+    //Cama 3 doble Grecia ------------------------------------------------------------- 3 doble
+    bt3dGrecia.addEventListener('click', function() {
+        if(bt3dGrecia.value == 0) {
+            GreciaIn += 1;
+            bt3dGrecia.value = 1;
+            bt3dGrecia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt3dGrecia.value == 1){
+            bt3dGrecia.value = 2;
+            bt3dGrecia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt3dGrecia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            GreciaIn -= 1;
+            bt3dGrecia.value = 0;
+            bt3dGrecia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });  
+    
+    //Cama 1 Grecia ---------------------------------------------------------------- 1
+    bt1Grecia.addEventListener('click', function() {
+        if(bt1Grecia.value == 0) {
+            GreciaIn += 1;
+            bt1Grecia.value = 1;
+            bt1Grecia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt1Grecia.value == 1){
+            bt1Grecia.value = 2;
+            bt1Grecia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt1Grecia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            GreciaIn -= 1;
+            bt1Grecia.value = 0;
+            bt1Grecia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    }); 
+
+    //Cama 2 Grecia ---------------------------------------------------------------- 2
+    bt2Grecia.addEventListener('click', function() {
+        if(bt2Grecia.value == 0) {
+            GreciaIn += 1;
+            bt2Grecia.value = 1;
+            bt2Grecia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt2Grecia.value == 1){
+            bt2Grecia.value = 2;
+            bt2Grecia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt2Grecia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            GreciaIn -= 1;
+            bt2Grecia.value = 0;
+            bt2Grecia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });     
+
+    //Cama 3 Grecia ---------------------------------------------------------------- 3
+    bt3Grecia.addEventListener('click', function() {
+        if(bt3Grecia.value == 0) {
+            GreciaIn += 1;
+            bt3Grecia.value = 1;
+            bt3Grecia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt3Grecia.value == 1){
+            bt3Grecia.value = 2;
+            bt3Grecia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt3Grecia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            GreciaIn -= 1;
+            bt3Grecia.value = 0;
+            bt3Grecia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });  
+    
+    //Cama 4 Grecia ---------------------------------------------------------------- 4
+    bt4Grecia.addEventListener('click', function() {
+        if(bt4Grecia.value == 0) {
+            GreciaIn += 1;
+            bt4Grecia.value = 1;
+            bt4Grecia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt4Grecia.value == 1){
+            bt4Grecia.value = 2;
+            bt4Grecia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt4Grecia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            GreciaIn -= 1;
+            bt4Grecia.value = 0;
+            bt4Grecia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });  
+    
+    //Cama 5 Grecia ---------------------------------------------------------------- 5
+    bt5Grecia.addEventListener('click', function() {   
+        if(bt5Grecia.value == 0) {
+            GreciaIn += 1;
+            bt5Grecia.value = 1;
+            bt5Grecia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt5Grecia.value == 1){
+            bt5Grecia.value = 2;
+            bt5Grecia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt5Grecia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            GreciaIn -= 1;
+            bt5Grecia.value = 0;
+            bt5Grecia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    }); 
+
+    //Cama 6 Grecia ---------------------------------------------------------------- 6
+    bt6Grecia.addEventListener('click', function() {
+        if(bt6Grecia.value == 0) {
+            GreciaIn += 1;
+            bt6Grecia.value = 1;
+            bt6Grecia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt6Grecia.value == 1){
+            bt6Grecia.value = 2;
+            bt6Grecia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt6Grecia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            GreciaIn -= 1;
+            bt6Grecia.value = 0;
+            bt6Grecia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });   
+  
+});
---
+++ Js/olimpia.js
@@ -0,0 +1,116 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let OlimpiaIn = 0;
+
+    const bt1Olimpia = document.getElementById("bt1Olimpia");
+    const bt2Olimpia = document.getElementById("bt2Olimpia");
+    const bt3Olimpia = document.getElementById("bt3Olimpia");
+    const bt4Olimpia = document.getElementById("bt4Olimpia");
+    const bt5Olimpia = document.getElementById("bt5Olimpia");
+    const bt6Olimpia = document.getElementById("bt6Olimpia");
+
+    //Cama 1 Olimpia ---------------------------------------------------------------- 1
+    bt1Olimpia.addEventListener('click', function() {
+        if(bt1Olimpia.value == 0) {
+            OlimpiaIn += 1;
+            bt1Olimpia.value = 1;
+            console.log(bt1Olimpia.value)
+            bt1Olimpia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt1Olimpia.value == 1){
+            bt1Olimpia.value = 2;
+            bt1Olimpia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt1Olimpia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+            console.log(bt1Olimpia.value)
+        } else {
+            OlimpiaIn -= 1;
+            bt1Olimpia.value = 0;
+            bt1Olimpia.classList.remove('bg-out', 'text-white', 'border-inBorder');   
+            console.log(bt1Olimpia.value)           
+        }
+    });  
+
+    //Cama 2 Olimpia ---------------------------------------------------------------- 2
+    bt2Olimpia.addEventListener('click', function() {
+        if(bt2Olimpia.value == 0) {
+            OlimpiaIn += 1;
+            bt2Olimpia.value = 1;
+            bt2Olimpia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt2Olimpia.value == 1){
+            bt2Olimpia.value = 2;
+            bt2Olimpia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt2Olimpia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            OlimpiaIn -= 1;
+            bt2Olimpia.value = 0;
+            bt2Olimpia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });    
+
+    //Cama 3 Olimpia ---------------------------------------------------------------- 3
+    bt3Olimpia.addEventListener('click', function() {
+        if(bt3Olimpia.value == 0) {
+            OlimpiaIn += 1;
+            bt3Olimpia.value = 1;
+            bt3Olimpia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt3Olimpia.value == 1){
+            bt3Olimpia.value = 2;
+            bt3Olimpia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt3Olimpia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            OlimpiaIn -= 1;
+            bt3Olimpia.value = 0;
+            bt3Olimpia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });  
+    
+    //Cama 4 Olimpia ---------------------------------------------------------------- 4
+    bt4Olimpia.addEventListener('click', function() {
+        if(bt4Olimpia.value == 0) {
+            OlimpiaIn += 1;
+            bt4Olimpia.value = 1;
+            bt4Olimpia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt4Olimpia.value == 1){
+            bt4Olimpia.value = 2;
+            bt4Olimpia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt4Olimpia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            OlimpiaIn -= 1;
+            bt4Olimpia.value = 0;
+            bt4Olimpia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    }); 
+
+    //Cama 5 Olimpia ---------------------------------------------------------------- 5
+    bt5Olimpia.addEventListener('click', function() {
+        if(bt5Olimpia.value == 0) {
+            OlimpiaIn += 1;
+            bt5Olimpia.value = 1;
+            bt5Olimpia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt5Olimpia.value == 1){
+            bt5Olimpia.value = 2;
+            bt5Olimpia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt5Olimpia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            OlimpiaIn -= 1;
+            bt5Olimpia.value = 0;
+            bt5Olimpia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });     
+
+    //Cama 6 Olimpia ---------------------------------------------------------------- 6
+    bt6Olimpia.addEventListener('click', function() {
+        if(bt6Olimpia.value == 0) {
+            OlimpiaIn += 1;
+            bt6Olimpia.value = 1;
+            bt6Olimpia.classList.add('bg-in', 'text-white', 'border-inBorder');
+        } else if(bt6Olimpia.value == 1){
+            bt6Olimpia.value = 2;
+            bt6Olimpia.classList.remove('bg-in', 'text-white', 'border-inBorder');
+            bt6Olimpia.classList.add('bg-out', 'text-white', 'border-inBorder');  
+        } else {
+            OlimpiaIn -= 1;
+            bt6Olimpia.value = 0;
+            bt6Olimpia.classList.remove('bg-out', 'text-white', 'border-inBorder');             
+        }
+    });  
+     
+});
